12b08f61d0523b6a9525b7c1aa19a91879e40cc3
Julia Melnikova <fox5rafy@gmail.com>
2022-10-28 10:44:28 +0300

    Added dtor; wtore CHECK and ASSERT macro

--- list.c
+++ list.c
@@ -3,7 +3,8 @@
 #include "list.h"
 
 
-int list_ctor(list_t* list, int capacity)
+//=========================================================================
+int list_ctor(list_t* list, size_t capacity)
 {
     if(list == NULL)
     {
@@ -20,6 +21,7 @@ int list_ctor(list_t* list, int capacity)
     {
         list->nodes = NULL;
         list->capacity = 0;
+        list->free = NULL_INDEX;
     }
 
     list = (list_t*) calloc(capacity, sizeof(list_t));
@@ -29,16 +31,37 @@ int list_ctor(list_t* list, int capacity)
         return ERR_LIST_OUT_MEMORY;
     }
 
-    list->nodes[NULL_INDEX] = DATA_POISON;
-    list->nodes = capacity;
-    list_init_nodes();
+    list->nodes[NULL_INDEX].data = DATA_POISON;
+    list->capacity = capacity;
+    list->free = NULL_INDEX;
+    list_init_nodes(list, 1);
 }
 
-int list_init_nodes()
+//=========================================================================
+
+int list_init_nodes(list_t* list, listIndex_t start)
 {
+    for(int idx = start; idx < list->capacity; ++idx)
+    {
+        list->nodes[idx].data = DATA_POISON;
+        list->nodes[idx].next = idx + 1;
+        list->nodes[idx].prev = INDEX_POISON;
+    }
+    list->nodes[list->capacity].next = list->free;
 
+    return 0;
 }
-int list_dtor()
+
+//=========================================================================
+
+int list_dtor(list_t* list)
 {
-
+    free(list->nodes);
+    list->nodes = NULL;
+    list->capacity = SIZE_MAX;
+    list->free = NULL_INDEX;
+    
+    return 0;
 }
+
+//=========================================================================
--- list.h
+++ list.h
@@ -26,6 +26,7 @@ typedef struct node_t
 typedef struct list_t
 {  
     node_t* nodes;
+    listIndex_t free;
     size_t capacity;
 
 }list_t;
@@ -38,3 +39,28 @@ enum list_error_names
     ERR_LIST_NULL_POINTER     = -2,
     ERR_LIST_NEGATIVE_COUNT   = -1,
 };
+
+#define CHECK(condition, ret)                             \
+                                                          \
+    int condition__ = condition;                          \
+    if (!condition__)                                     \
+    {                                                     \
+        ASSERT(condition__, #condition);                  \
+        return ret;                                       \
+    }                                                     \
+
+#ifdef DEBUG
+    #define ASSERT(condition, message)                    \
+                                                          \
+    if (!condition)                                       \
+    {                                                     \
+        fprintf(stderr, "*** ASSERTION ERROR ***\n");     \
+        fprintf(stderr, "    Failed %s\n", message);      \
+        fprintf(stderr, "    Location:\n");               \
+        fprintf(stderr, "    File ----- %s\n", __FILE__); \
+        fprintf(stderr, "    Function - %s\n", __func__); \
+        fprintf(stderr, "    Line ----- %d\n", __LINE__); \
+    }                                                     \
+#else
+    #define ASSERT(condition, message){}
+#endif
