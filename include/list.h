22c7e4d9f42e367e426383a5c30d632e5a0bc2c7
Julia Melnikova <fox5rafy@gmail.com>
2022-10-31 17:29:51 +0300

    Added macro LIST_OK; LIST_ERROR; changed func list_dump - now print the errors

--- include/list.h
+++ include/list.h
@@ -74,6 +74,56 @@ enum list_codes
     #define ASSERT(condition, message, retval){}
 #endif
 
+
+//! Macros LIST_ERROR
+/*! macros to print the error. */
+#define LIST_ERROR(ERROR_NAME)                                                      \
+{                                                                                   \
+    fprintf(stderr, RED "ERROR:" RESET "line %d. " #ERROR_NAME "\n", __LINE__);     \
+};
+
+//! Macros LIST_OK
+/*! macros to describe the error. */
+#define LIST_OK(list)                                                               \
+                                                                                    \
+    ret_ = stack_verify(list);                                                      \
+                                                                                    \
+    switch (ret_)                                                                   \
+    {                                                                               \
+    case (1 << ERR_LIST_NULL_PTR):                                                  \
+        LIST_ERROR(ERR_LIST_NULL_PTR);                                              \
+        break;                                                                      \
+                                                                                    \
+    case (1 << ERR_LIST_OUT_MEMORY):                                                \
+        LIST_ERROR(ERR_LIST_OUT_MEMORY);                                            \
+        break;                                                                      \
+                                                                                    \
+    case (1 << ERR_LIST_UNDERFLOW):                                                 \
+        LIST_ERROR(ERR_LIST_UNDERFLOW);                                             \
+        break;                                                                      \
+                                                                                    \
+    case (1 << ERR_LIST_OVERFLOW):                                                  \
+        LIST_ERROR(ERR_LIST_OVERFLOW);                                              \
+        break;                                                                      \
+                                                                                    \
+    case (1 << ERR_LIST_BAD_POSITION):                                              \
+        LIST_ERROR(ERR_LIST_BAD_POSITION);                                          \
+        break;                                                                      \
+                                                                                    \
+    case (1 << ERR_LIST_BAD_PTR):                                                   \
+        LIST_ERROR(ERR_LIST_BAD_PTR);                                               \
+        break;                                                                      \
+                                                                                    \
+    case (1 << ERR_LIST_BAD_PTR):                                                   \
+        LIST_ERROR(ERR_LIST_BAD_PTR);                                               \
+        break;                                                                      \
+                                                                                    \
+    case (1 << ERR_LIST_INC_LIST):                                                  \
+        LIST_ERROR(ERR_LIST_INC_LIST);                                              \
+        break;                                                                      \
+    };                                                          
+
+
 int list_ctor(list_t* list, size_t capacity);
 int list_init_data(list_t* list);
 int list_dtor(list_t* list);
