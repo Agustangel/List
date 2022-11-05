736aadeec029bf98e09d4824b5ed73bc2f73481c
Julia Melnikova <fox5rafy@gmail.com>
2022-11-05 21:14:00 +0300

    Added documentation

--- include/list.h
+++ include/list.h
@@ -7,12 +7,22 @@
 
 
 //=========================================================================
-
+//! constant for indicates free position
 #define FREE_INDEX -1
+
+//! constant for indicates poison index
 #define INDEX_POISON -666
+
+//! constant for indicates start index
 #define START_INDEX 1
+
+//! constant for indicates poison data in list
 #define DATA_POISON NAN
+
+//! constant for indicates the first index in arrays
 #define NULL_INDEX 0
+
+//! constant for indicates numbers of errors
 #define ERROR_NUMBER 8
 
 //=========================================================================
@@ -21,27 +31,28 @@ typedef double elem_t;
 typedef int listIndex_t;
 typedef uint_fast16_t listStatus_t;
 
+//!  A structure of list
 typedef struct list_t
 {  
-    elem_t* data;
-    listIndex_t* next;
-    listIndex_t* prev;
+    elem_t* data;           /**< array of the data */
+    listIndex_t* next;      /**< array of the next indices */
+    listIndex_t* prev;      /**< array of the prev indices */
 
-    listIndex_t free;
-    listIndex_t end;
-    listIndex_t head;
-    listIndex_t tail;
+    listIndex_t free;       /**< the first free  index */
+    listIndex_t end;        /**< the end logical index */
+    listIndex_t head;       /**< the first physical index */
+    listIndex_t tail;       /**< the end   physical index */
     
-    size_t size;
-    size_t capacity;
+    size_t size;            /**< size of list */
+    size_t capacity;        /**< capacity of list */
 
-    listStatus_t status;
-    bool linflag;
+    listStatus_t status;    /**< contains information about errors */ 
+    bool linflag;           /**< linearization flag */
     
 }list_t;
 
 //=========================================================================
-
+/*! enumeration of list errors and list success with values. */
 enum list_codes
 {
     LIST_SUCCESS              = 0,
@@ -57,16 +68,75 @@ enum list_codes
 
 //=========================================================================
 
+//! Сonstructor function
+/*! \param list - pointer to list on stack.
+    \param capacity - initial capacity of list.
+ *  \returns result - list_codes. */
 int list_ctor(list_t* list, size_t capacity);
+
+//! Function that initializes the list data
+/*! \param list - pointer to list on stack.
+ *  \returns result - list_codes. */
 int list_init_data(list_t* list);
+
+//! Destructor function
+/*! \param list - pointer to list on stack.
+ *  \returns result - list_codes. */
 int list_dtor(list_t* list);
+
+//! Function that prints all information about list
+/*! \param list - pointer to list on stack.
+ *  \returns result - list_codes. */
 int list_dump(list_t* list);
+
+//! Function that creates new node at the end of list
+/*! \param list - pointer to list on stack.
+    \param value - value of element at the end of list.
+ *  \returns result - list_codes. */
 int push_back(list_t* list, elem_t value);
+
+//! Function that deletes node at the end of list
+/*! \param list - pointer to list on stack.
+ *  \returns result - list_codes. */
 int pop_back(list_t* list);
+
+//! Function that changes the size of list
+/*! \param list - pointer to list on stack.
+ *  \returns result - list_codes. */
 int list_resize(list_t* list);
+
+//! Function that inserts element after specified position
+/*! \param list - pointer to list on stack.
+    \param lognum - logical number of element.
+    \param value - value of the inserted element.
+ *  \returns result - list_codes. */
 int insert_after(list_t* list, listIndex_t lognum, elem_t value);
+
+//! Function that inserts element before specified position
+/*! \param list - pointer to list on stack.
+    \param lognum - logical number of element.
+    \param value - value of the inserted element.
+ *  \returns result - list_codes. */
 int insert_before(list_t* list, listIndex_t lognum, elem_t value);
+
+//! Function that gets physical number from logical
+/*! \param list - pointer to list on stack.
+    \param lognum - logical number of element.
+ *  \returns result - list_codes. */
 int get_physical_number(list_t* list, listIndex_t lognum);
+
+//! Function that deletes element specified position
+/*! \param list - pointer to list on stack.
+    \param lognum - logical number of element.
+ *  \returns result - list_codes. */
 int delete_elem(list_t* list, listIndex_t lognum);
+
+//! Function that looks for all errors of list
+/*! \param list - pointer to list on stack.
+ *  \returns result - list_codes. */
 int list_veryfi(list_t* list);
+
+//! Function that shows all information about list graphically
+/*! \param list - pointer to list on stack.
+ *  \returns result - list_codes. */
 int graph_dump(list_t* list);
